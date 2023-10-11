044377c131decb903d8eb790ab2823fce6ff5734
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-11 08:41:31 -0300

    add cancel o products/create

--- src/app/components/product/create.ts
+++ src/app/components/product/create.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './product.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-create',
   template: `
   <p>Create Works!</p>
   <button mat-raised-button (click)="createProduct()" color="primary">Salvar</button>
+  <button mat-raised-button (click)="cancel()" >Cancelar</button>
   `
 })
 export class Create implements OnInit{
-  constructor(private productService: ProductService) {  }
+  constructor(private productService: ProductService, private router: Router) {  }
 
 
 
@@ -17,7 +19,6 @@ export class Create implements OnInit{
     // console.log(this.productService.array)
   }
 
-  createProduct(): void {
-    this.productService.showMessage('Teste...123')
-  }
+  createProduct(): void { this.productService.showMessage('Produto criado')}
+  cancel(): void { this.router.navigate(['products'])}
 }
