8635fea486b47af580d48ab1da11e3567434bf82
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-13 12:27:41 -0300

    errorHandler on every method

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -12,7 +12,7 @@ export class ProductService {
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
   showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
-      duration: 3000000,
+      duration: 3000,
       horizontalPosition: 'right',
       verticalPosition: 'top',
       panelClass: isError ? ['msg-error'] : ['msg-success'] 
@@ -21,20 +21,20 @@ export class ProductService {
   create(product: Product): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product).pipe(map(_ => _), catchError(e => this.errorHandler(e)))
   }
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl).pipe(map(_ => _), catchError(e => this.errorHandler(e)))
+  }
+  readById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`).pipe(map(_ => _), catchError(e => this.errorHandler(e)))
+  }
+  update(product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.baseUrl}/${product.id}`, product).pipe(map(_ => _), catchError(e => this.errorHandler(e)))
+  }
+  delete(id: number): Observable<Product> {
+    return this.http.delete<Product>(`${this.baseUrl}/${id}`).pipe(map(_ => _), catchError(e => this.errorHandler(e)))
+  }
   errorHandler(e: any): Observable<any>{
     this.showMessage('Ocorreu um erro!', true)
     return EMPTY
-  }
-  read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl)
-  }
-  readById(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.baseUrl}/${id}`)
-  }
-  update(product: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.baseUrl}/${product.id}`, product)
-  }
-  delete(id: number): Observable<Product> {
-    return this.http.delete<Product>(`${this.baseUrl}/${id}`)
   }
 }
