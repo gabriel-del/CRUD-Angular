ffdc580fdc285d66317cbe5f3e47829b4ad7b076
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-12 21:54:08 -0300

    add icons edit and delete

--- src/app/components/product/read.ts
+++ src/app/components/product/read.ts
@@ -18,14 +18,25 @@ import { Product } from './model';
           <th mat-header-cell *matHeaderCellDef>Preço</th>
           <td mat-cell *matCellDef="let row">{{row.price | currency: 'BRL' }}</td>
         </ng-container>
+        <ng-container matColumnDef="action">
+          <th mat-header-cell *matHeaderCellDef>Ações</th>
+          <td mat-cell *matCellDef="let row">
+            <a routerLink=""><i class="material-icons">edit</i></a>
+            <a routerLink=""><i class="material-icons">delete</i></a>
+          </td>
+        </ng-container>
         <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
         <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
       </table>
+      <style>
+        a:nth-child(1) { color: #d9cd26; margin-right: 10px;}
+        a:nth-child(2) { color: #e35e6b;}
+      </style>
   `
 })
 export class Read implements OnInit {
   products!: Product[]
-  displayedColumns = ['id', 'name', 'price']
+  displayedColumns = ['id', 'name', 'price', 'action']
 
   constructor (private productService: ProductService) {}
   ngOnInit (): void {
