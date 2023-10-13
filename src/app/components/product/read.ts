8bbe6202593c23ded3c653fbd17bb47e72021a97
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-12 21:15:21 -0300

    currency BRL

--- src/app/components/product/read.ts
+++ src/app/components/product/read.ts
@@ -5,8 +5,7 @@ import { Product } from './model';
 @Component({
   selector: 'app-read',
   template: `
-    <div class="mat-elevation-z4">
-      <table mat-table [dataSource]="products">
+      <table mat-table [dataSource]="products" class="mat-elevation-z4">
         <ng-container matColumnDef="id">
           <th mat-header-cell *matHeaderCellDef>Id</th>
           <td mat-cell *matCellDef="let row">{{row.id}}</td>
@@ -17,12 +16,11 @@ import { Product } from './model';
         </ng-container>
         <ng-container matColumnDef="price">
           <th mat-header-cell *matHeaderCellDef>Preço</th>
-          <td mat-cell *matCellDef="let row">{{row.price}}</td>
+          <td mat-cell *matCellDef="let row">{{row.price | currency: 'BRL' }}</td>
         </ng-container>
         <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
         <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
       </table>
-    </div>
   `
 })
 export class Read implements OnInit {
